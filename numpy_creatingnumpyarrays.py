ff5c8289928274ebc83264ef0fe96d937d8a2300
Merve Gamze Çınar <62151645+mgamzec@users.noreply.github.com>
2023-02-23 23:18:51 +0300

    Add files via upload

---
+++ numpy_creatingnumpyarrays.py
@@ -0,0 +1,10 @@
+# Creating NumPy Arrays
+
+import numpy as np
+np.array([1,2,3,4])
+type(np.array([1,2,3,4]))
+
+
+np.zeros(10, dtype=int)
+np.random.randint(0, 10, size=10)
+np.random.normal(10, 4, (3,4))
